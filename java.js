222007c463a095f5ca1bedbf73111f9bdcf95279
George-Stephen <yazz777stevie@gmail.com>
2020-02-13 12:34:07 +0300

    new data

--- java.js
+++ java.js
@@ -1,13 +1,10 @@
 function calc(){
     alert("calculating....")
     var Century,Year,Month,Day,gender,Date;
-    Century =document.getElementsByClassName('century').value;
-    Year =document.getElementsByClassName('year').value;
-    Month =document.getElementsByClassName('month').value;
-    Day =document.getElementsByClassName('day').value;
+    Century = ParseInt(document.getElementsByClassName('century').value;)
+    Year =ParseInt(document.getElementsByClassName('year').value;)
+    Month =ParseIntdocument.getElementsByClassName('month').value;)
+    Day =ParseInt(document.getElementsByClassName('day').value;)
     gender =document.getElementsByClassName('gender').value;
-    document.getElementById('demo').innerHTML= (gender)
-    Date =( ( (Century/4) -2*Century-1) + ((5*Year/4) ) + ((26*(Month+1)/10)) + Day ) mod 7 ;
-    alert(Date)
 
 };
