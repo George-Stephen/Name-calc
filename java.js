793583f626895563de485c75189ebe2b32c48def
George-Stephen <yazz777stevie@gmail.com>
2020-02-13 16:00:19 +0300

    new

--- java.js
+++ java.js
@@ -4,26 +4,24 @@ function calc(){
     var maleName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",];
     var femaleName =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
 
-    alert("calculating ...")
     var Year =parseInt(document.getElementById("year").value);
-    var Month =parseInt(document.getElementById("month").value);
+    var Month =parseInt(document.getElementById('month').value);
     var Day =parseInt(document.getElementById("day").value);
     var gender =document.getElementById("gender").value;
 
     if(Month<=0||Month >12){
         alert("Add a correct month!!!");
-
     }
     else if(Day <=1 ||Day>31){
         alert("Add the correct day !!!")
     }
-    
-    var day= new Date(Year+"/"+ Month+"/"+day);
+    var day= new Date(Year +"/"+ Month+"/"+ day);
     var BirthDay=day.getDay();
 
-    if(gender=="male"){
-        alert("your name is"+maleName[BirthDay] )
-    } else if(gender=="female"){
-        alert("Your name is"+femaleName[BirthDay] )
+    if(Male.checked==true){
+        alert("your name is"+maleName[BirthDay])
+    }
+    if(Female.checked==true){
+        alert("Your name is"+femaleName[BirthDay])
     }
 }
