5555883e08e60195f3a519f82675a6d24696d9a4
George-Stephen <yazz777stevie@gmail.com>
2020-02-13 15:05:34 +0300

    new

--- java.js
+++ java.js
@@ -1,18 +1,16 @@
 function calc(){
-
-
     //-Names--/
     var Days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
     var maleName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",];
     var femaleName =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
 
     alert("calculating ...")
-    var Year =ParseInt(document.getElementsByid("year").value);
-    var Month =ParseInt(document.getElementsByid("month").value);
-    var Day =ParseInt(document.getElementsByid("day").value);
+    var Year =parseInt(document.getElementsByid("year").value);
+    var Month =parseInt(document.getElementsByid("month").value);
+    var Day =parseInt(document.getElementsByid("day").value);
     var gender =document.getElementsByid("gender").value;
 
-    if(Month<=0||Month>12){
+    if(Month<=0||Month >12){
         alert("Add a correct month!!!");
 
     }
@@ -21,10 +19,11 @@ function calc(){
     }
     
     var day= new Date(Year+"/"+ Month+"/"+day);
-    var Birthday= day.getday() 
+    var BirthDay=day.getDay();
+
     if(gender=="male"){
-        alert("your name is"+ maleName[Birthday] )
+        alert("your name is"+maleName[BirthDay] )
     } else if(gender=="female"){
-        alert("Your name is"+ femaleName[Birthday] )
+        alert("Your name is"+femaleName[BirthDay] )
     }
 }
