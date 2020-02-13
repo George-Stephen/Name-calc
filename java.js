43dc43c11b27e5fcdc3abe19b9a7f61d9e539168
George-Stephen <yazz777stevie@gmail.com>
2020-02-13 14:04:43 +0300

    new

--- java.js
+++ java.js
@@ -1,10 +1,30 @@
 function calc(){
+    //-Names--/
+    var Days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
+    var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",];
+    var femaleName =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
+    
     alert("calculating....")
-    var Century,Year,Month,Day,gender,Date;
-    Century = ParseInt(document.getElementsByClassName('century').value;)
-    Year =ParseInt(document.getElementsByClassName('year').value;)
-    Month =ParseIntdocument.getElementsByClassName('month').value;)
-    Day =ParseInt(document.getElementsByClassName('day').value;)
+    //-Variable--//
+    var Year,Month,Day,gender,;
+    Year =ParseInt(document.getElementsByClassName('year').value);
+    Month =ParseInt(document.getElementsByClassName('month').value);
+    Day =ParseInt(document.getElementsByClassName('day').value);
     gender =document.getElementsByClassName('gender').value;
+    //--IF functions--//
+    if(Month<=0||Month>12){
+        alert("Add a correct month!!!");
+    }
+    else if(Day <=1 ||Day>31){
+        alert("Add the correct day !!!")
+    }
 
-};
+    var day=new Date(Year+"/"+ Month+"/"+day);
+    var Birthday=day.getDay();
+
+    if(gender=="male"){
+        alert("your name is"+maleNames[maleNames])
+    } else if(gender=="female"){
+        alert("Your name is"+femaleNames[femaleNames])
+    }
+}
